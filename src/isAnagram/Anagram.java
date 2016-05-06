90721b9c3beca2755e12c118373338dad6ca3cf7
Jenny <ycharnetskaya@gmail.com>
2016-05-05 20:42:51 -0400

    feat:: check if anagram algorithm build

---
+++ src/isAnagram/Anagram.java
@@ -0,0 +1,36 @@
+package isAnagram;
+
+public class Anagram {
+	
+	public boolean isAnagram(String str1, String str2){
+		
+		if(str1.length() != str2.length()){
+			return false;
+		}
+		
+		char[] chars = str1.toCharArray();
+		
+		for (char c: chars){
+			int index = str2.indexOf(c);
+			
+			if (index != -1){
+				str2 = str2.substring(0, index) + str2.substring(index + 1, str2.length());
+			}else{
+				return false;
+			}
+		}
+		
+		return str2.isEmpty();
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Anagram anagram = new Anagram();
+		
+		System.out.println(anagram.isAnagram("word", "wrdo"));
+		System.out.println(anagram.isAnagram("worfd", "hwrdo"));
+	}
+
+}
