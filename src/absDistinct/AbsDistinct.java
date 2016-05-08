2ddd3c0dd3f88bb05a64a2cbe26742d02542aacb
Jenny <ycharnetskaya@gmail.com>
2016-05-08 11:39:24 -0400

    feat: found Caterpillar solution
    the reason why the caterpillar works here is because array is sorted and
    uses abstract elements only

--- src/absDistinct/AbsDistinct.java
+++ src/absDistinct/AbsDistinct.java
@@ -15,6 +15,24 @@ public class AbsDistinct {
 		}
 		return map.size();
 	}
+	
+	public int solutionCaterpillar(int[] A) {
+		int count = 0;
+		for (int i = 0, j = A.length - 1, prev = -1; i <= j; ) {
+			int left = Math.abs(A[i]), right = Math.abs(A[j]);
+			int max = left == Integer.MIN_VALUE ? left : Math.max(left, right);
+			if (prev != max) {
+				count++;
+			}
+			prev = max;
+			if (left == Integer.MIN_VALUE || right < left) {
+				i++;
+			} else {
+				j--;
+			}
+		}
+		return count;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -22,6 +40,7 @@ public class AbsDistinct {
 		AbsDistinct abs = new AbsDistinct();
 		int [] array = {-5,-3,-1,0,3,6};
 		System.out.println(abs.solution(array));
+		System.out.println(abs.solutionCaterpillar(array));
 	}
 
 }
