278258d95384c43653a0c6641adb9b27f9d91981
Jenny <ycharnetskaya@gmail.com>
2016-05-16 18:07:17 -0400

    feat: added JUnit test to test LinkedList class
    tests include insert

---
+++ src/linkedList/LinkedListTest.java
@@ -0,0 +1,48 @@
+package linkedList;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LinkedListTest {
+	
+	private LinkedList list;
+
+	@Before
+	public void setUp() throws Exception {
+		this.list = new LinkedList();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		this.list = null;
+	}
+
+	@Test
+	public void testListEmpty() {
+		assertTrue(this.list.getHead() == null);
+	}
+	
+	@Test
+	public void testInsertEmptyList(){
+		
+		Node newHead = this.list.insert(list.getHead(), 1);
+		assertTrue(newHead.getValue() == 1);
+	}
+	
+	@Test
+	public void testInsertMultipleElements(){
+		
+		Node newHead = this.list.insert(list.getHead(), 1);
+		Node newHead1 = this.list.insert(newHead, 2);
+		Node newHead2 = this.list.insert(newHead1, 3);
+		
+		assertTrue(newHead2.getValue() == 3);
+		assertTrue(newHead2.getNextNode().getValue() == 2);
+		assertTrue(newHead2.getNextNode().getNextNode().getValue() == 1);
+		assertTrue(newHead2.getNextNode().getNextNode().getNextNode() == null);
+	}
+
+}
